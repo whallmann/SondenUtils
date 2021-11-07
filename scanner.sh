2ad399dc162cda38648406dd93f6692c2ac0af99
Antennen-Wolfgang <df7pn@darc.de>
2021-11-07 15:23:32 +0100

    deleted
    no longer good for use

--- scanner.sh
+++
@@ -1,4 +0,0 @@
-rtl_power -f 400M:406M:1000 -d0 -g 38 -p 28 /tmp/scan.csv -1 2>&1 > /dev/null
-cd /home/pi
-sudo ./scannerlist -L -30 -H /tmp/holding.txt -o /tmp/outlist.txt -f /tmp/scan.csv -v -d /home/pi/scannerblacklist.txt -w /home/pi/scannerwhitelist.txt  > /tmp/scannerlist.log
-
